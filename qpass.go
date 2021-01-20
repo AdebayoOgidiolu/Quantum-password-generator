c23bcfa2b00f50f13198ce04b684c70eabd89e79
Adebayo Ogidiolu <ogidioluadebayo@gmail.com>
2021-01-20 18:54:42 +0100

    Added Unicode Characters

--- qpass.go
+++ qpass.go
@@ -3,8 +3,14 @@ package qpass
 import (
 	"math/rand"
 	"strings"
+	"unicode"
 
 	"github.com/bitfield/qrand"
+)
+
+const (
+	Typeable = iota
+	Unicode
 )
 
 func NewPassword(length int) string {
@@ -12,19 +18,35 @@ func NewPassword(length int) string {
 }
 
 type generator struct {
-	Rand *rand.Rand
+	Rand         *rand.Rand
+	CharacterSet int
 }
 
 func NewGenerator() generator {
 	return generator{
-		Rand: rand.New(qrand.NewSource()),
+		Rand:         rand.New(qrand.NewSource()),
+		CharacterSet: Typeable,
 	}
+}
+
+func (g generator) typeableRune() rune {
+	return rune(' ' + g.Rand.Intn(95))
+}
+
+func (g generator) unicodeRune() rune {
+	return rune(g.Rand.Intn(unicode.MaxRune + 1))
 }
 
 func (g generator) NewPassword(length int) string {
 	var s strings.Builder
+	var newRune func() rune
+	if g.CharacterSet == Unicode {
+		newRune = g.unicodeRune
+	} else {
+		newRune = g.typeableRune
+	}
 	for i := 0; i < length; i++ {
-		s.WriteRune(' ' + rune(g.Rand.Intn(95)))
+		s.WriteRune(newRune())
 	}
 	return s.String()
 }
